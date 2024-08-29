2ae6175e13646404d55ce7b6fcd828dd6eb351a4
tmi.tuta <tmi.tutaa@gmail.com>
2024-08-29 17:23:46 +0700

    update not found file review

--- app/books/[bookId]/reviews/[reviewId]/page.tsx
+++ app/books/[bookId]/reviews/[reviewId]/page.tsx
@@ -1,3 +1,5 @@
+import { notFound } from "next/navigation";
+
 export default function reviewDetail({
     params,
 }: {
@@ -6,6 +8,9 @@ export default function reviewDetail({
         reviewId: string;
     };
 }) {
+    if (parseInt(params.bookId) > 1000) {
+        notFound();
+    }
     return (
         <h1>Đánh giá { params.reviewId } Cho sách có id: { params.bookId }</h1>
     )
